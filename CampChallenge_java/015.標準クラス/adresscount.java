34f3d0fcb1067548825fca4261578ff23820fb7f
yuhei yoshinari <kangaskhanlover7@gmail.com>
2018-07-12 14:26:40 +0900

    文字数の取得課題提出します

---
+++ CampChallenge_java/015.標準クラス/adresscount.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class adresscount {
+    public static void main(String[] args){
+        String adress = "kangaskhanlover7@gmail.com";
+        System.out.println(adress.substring(16).length());
+    }
+}
